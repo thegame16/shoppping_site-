8b71a4df740e83330b60de1400fec4bbddcb0e57
thegame16 <132762153+thegame16@users.noreply.github.com>
2026-03-09 22:52:12 +0400

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to ShopEase</h1>
+      <p>Your one-stop online store for everything!</p>
+    </div>
+  );
+};
+
+export default Home;
